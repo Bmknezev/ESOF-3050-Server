50f839ecf84c44509839ccf99c379cecdf224bea
braydon <bmknezev@lakeheadu.ca>
2023-11-13 09:03:10 -0500

    added second light for testing

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         SmartLight l = new SmartLight("light 1", 1,true, -1,true,0x000000,100,true );
+        SmartLight l2 = new SmartLight("light 2", 2,true, -1,true,0x000000,100,true );
 
         SmartHomeServer s = new SmartHomeServer(19920);
         s.newDevice(l);
+        s.newDevice(l2);
 
         try {
             s.listen();
